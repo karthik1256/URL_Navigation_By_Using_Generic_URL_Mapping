2efabadb0aa830d8cf5a6018a9d7316a34362f78
karthik1256 <lkarthi1256@gmail.com>
2023-11-20 21:26:56 +0530

    first commit

---
+++ apple/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def karthik(request):
+    return render(request,'karthik.html')
+
+
+def pandu(request):
+    return render(request,'pandu.html')
